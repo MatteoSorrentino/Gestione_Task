34e7f7725ef6885977ad956f5a8548883addda0e
Matteo Sorrentino <Ciro & Luisa@DESKTOP-1GLGU9U>
2019-11-18 16:34:12 +0100

    Aggiungere i file di progetto.

---
+++ GestioneTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GestioneTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //costruzione di un semplice task
+            var simpleTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Ciao da simpleTask");
+            });
+
+            //costruzione di un task con parametro in input
+            var parameterTask = Task.Factory.StartNew((name) =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Ciao da parameterTask, {0}", name);
+            }, "Matteo Tumiati");
+
+            //costruzione di un task che ritorna un risultato
+            var resultTask = Task.Factory.StartNew((inputValue) =>
+                        PerformSomeLongCalculation(inputValue), 5000D);
+        }
+    }
+}
